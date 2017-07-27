fa6389e038ed2c0f109f5971830713de820b72d0
Miralem Cebic <miralem.cebic@matrix42.com>
2017-07-27 12:08:34 +0200

    App is Ready

--- MixingCppObjC/MixingCppObjC/ViewController.mm
+++ MixingCppObjC/MixingCppObjC/ViewController.mm
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#include "Cat.hpp"
 
 @interface ViewController ()
 
@@ -25,5 +26,21 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)executeCatCode:(UIButton *)sender {
+    NSLog(@"Exec Cat.cpp");
+    
+    int Age;
+    std::cout<<"How old is Frisky? ";
+    std::cin>>Age;
+    Cat Frisky(Age);
+    Frisky.Meow();
+    std::cout << "Frisky is a cat who is " ;
+    std::cout << Frisky.GetAge() << " years old.\n";
+    Frisky.Meow();
+    Age++;
+    Frisky.SetAge(Age);
+    std::cout << "Now Frisky is ";
+    std::cout << Frisky.GetAge() << " years old.\n";
+}
 
 @end
